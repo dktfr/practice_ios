f607365e8136fcca90fa6148dc2852513c49a291
dktfr <dktfr@naver.com>
2016-04-08 02:07:37 +0900

    Add Group & Asset. But...... Apple Did not provide delete............................

---
+++ PracticeProject2/InvalidateDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UIViewController+Invalidator.h
+//  PracticeProject2
+//
+//  Created by NHNEnt on 2016. 4. 7..
+//  Copyright © 2016년 NHNEnt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Invalidator)
+
+- (void)invalidateView;
+
+@end
